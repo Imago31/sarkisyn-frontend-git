2fc56abfcf2fbde4b4fb2c969af613ee6efbae34
Imago31 <xaker201301@gmail.com>
2022-04-02 21:36:11 +0200

    git_lesson_commit_4

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+
+import '../css/style.css';
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,69 @@
 /* eslint-disable prettier/prettier */
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-  entry: "./src/index.js",
-  mode,
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      '...',
-      new CssMinimizerPlugin(),
-    ],
-  },
-  devtool:
-    process.env.NODE_ENV === "production" ? false : "source-map",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
-};
+    entry: './src/index.ts',
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+        template: './index.html',
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'game-level-1.html',
+        template: './game-level-1.html',
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'game-level-2.html',
+        template: './game-level-2.html',
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'game-level-3.html',
+        template: './game-level-3.html',
+    }),
+    new CopyPlugin({
+        patterns: [
+          { from: "img", to: "img" },
+        ],
+      }),
+],
+}
